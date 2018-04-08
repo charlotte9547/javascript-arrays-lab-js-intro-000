07408e57edd84a365c5af4c9db15afca4b9683f0
Xingxin Chen <xingxinc@andrew.cmu.edu>
2018-04-08 22:26:24 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const app = "I don't do much."
 var kittens =["Milo","Otis","Garfield"];
-function destructivelyAppendKitten (kittens){
-  appendKitten()
-  return kittens
+function destructivelyAppendKitten (name){
+  kittens.push(name)
 }
